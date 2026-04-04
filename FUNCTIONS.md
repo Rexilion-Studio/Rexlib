8d7610b3239646ba765a6a533912ffc011086147
Fejtrix <samkodugas2902@gmail.com>
2026-04-04 12:10:45 +0200

    Release Preparations

---
+++ FUNCTIONS.md
@@ -0,0 +1,25 @@
+# RexLib
+ A Lua library to make coding easier and more time efficient!
+ Rexlib is a free to use, royalty free library, it is very simple to use,
+ Rexlib commands look like this:
+ Rexlib."commandname(--insert value if needed)"
+Rexlib Commands:
+  Rexlib.inPercent(value,maxvalue)--returns a percentage
+  Rexlib.cloneTable(table)--creates a clone of a table
+  Rexlib.wait(seconds)--waits a certain ammount of seconds before running more code, ussualy you want to run it in background using rexlib.runinBackground()
+  Rexlib.filter(table to filter,condition to check for for every filtered value)--filters table by condition
+  Rexlib.repeatFunction(function to repeat,how many times to repeat)--repeats a function certain ammount of time, if set to -1 it will repeat forever
+  Rexlib.removeFromTable(table to remove from,table of things to find and remove)--removes a list of values from a table
+  Rexlib.listTable(table)--prints everything inside of the table
+  Rexlib.runinBackground(function to run in background)--runs the function in a coroutine, so all other functions can continue
+  Rexlib.changed(function that is linked to checking the value,a boolean to be switched uppon value being changed)--bit more complicated, but basicly create a function that will return a value or just use rexlib.getValue, and then put that function inside this function, and it will return a table with boolean saying that the value has been changed once the value changes
+  Rexlib.getValue(Value)--returns value, usefull if you need to check value mid loop
+Rexlib Managment Comands:
+  Rexlib.clearWatchers()--cleans the watcher table, essentialy nuking all current rexlib functions that rely on them, do this only if absolutely neccessary or when shutting down all rexlib funtions at once
+
+
+This was our first major project, we had to learn throuhg many sources such as github, Ai and other forums so this may not be perfect, but we believe it is good enough to make you save a lot of time in coding
+
+CERTAIN REXLIB FUNCTIONS ARE REQUIRED TO RUN OTHER REXLIB FUNCTIONS!!!
+
+--Made by Seb and Fej
---
+++ install.bat
@@ -0,0 +1,111 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM === CONFIG ===
+set REPO_URL=https://raw.githubusercontent.com/SebCodesHere/RexLib/main/rexlib.lua
+set INSTALL_DIR=%USERPROFILE%\.rexlib
+set TARGET=%INSTALL_DIR%\rexlib.lua
+set TEMP_FILE=%TEMP%\rexlib_new.lua
+
+title RexLib Installer
+color 0A
+
+echo.
+echo =====================================
+echo        RexLib Installer
+echo =====================================
+echo.
+
+call :progressBar 5 "Initializing..."
+
+REM === CHECK CURL ===
+where curl >nul 2>nul
+if %errorlevel% neq 0 (
+    echo.
+    echo [ERROR] curl is not installed.
+    pause
+    exit /b
+)
+
+REM === ENSURE DIRECTORY EXISTS ===
+if not exist "%INSTALL_DIR%" (
+    call :progressBar 5 "Creating installation directory..."
+    mkdir "%INSTALL_DIR%"
+)
+
+REM === DOWNLOAD ===
+call :progressBar 5 "Downloading latest version..."
+curl -L -o "%TEMP_FILE%" "%REPO_URL%" >nul 2>nul
+
+if %errorlevel% neq 0 (
+    echo.
+    echo [ERROR] Download failed.
+    pause
+    exit /b
+)
+
+REM === VERIFY ===
+call :progressBar 5 "Verifying files..."
+
+if exist "%TARGET%" (
+    for %%A in ("%TARGET%") do set OLD_TIME=%%~tA
+    for %%B in ("%TEMP_FILE%") do set NEW_TIME=%%~tB
+
+    if "!OLD_TIME!"=="!NEW_TIME!" (
+        echo.
+        echo [INFO] RexLib is already up to date.
+        del "%TEMP_FILE%" >nul 2>nul
+        goto done
+    ) else (
+        echo.
+        echo [INFO] Update found. Replacing old version...
+        call :progressBar 5 "Updating..."
+        del "%TARGET%" >nul 2>nul
+    )
+) else (
+    echo.
+    echo [INFO] Installing RexLib for the first time...
+    call :progressBar 5 "Installing..."
+)
+
+REM === INSTALL NEW FILE ===
+move /Y "%TEMP_FILE%" "%TARGET%" >nul 2>nul
+
+call :progressBar 5 "Finalizing..."
+
+:done
+echo.
+echo =====================================
+echo   RexLib is ready!
+echo   Path: %TARGET%
+echo =====================================
+echo.
+
+pause
+exit /b
+
+REM =========================
+REM 5-SECOND PROGRESS BAR
+REM =========================
+:progressBar
+set seconds=%~1
+set message=%~2
+set barLength=25
+
+echo %message%
+
+for /l %%i in (1,1,%barLength%) do (
+    set /a percent=%%i*100/%barLength%
+
+    set bar=
+    for /l %%a in (1,1,%%i) do set bar=!bar!#
+    for /l %%b in (%%i,1,%barLength%) do set bar=!bar!-
+
+    <nul set /p= [!bar!] !percent!%%`r
+
+    set /a wait=%seconds%*1000/%barLength%
+    powershell -command "Start-Sleep -Milliseconds !wait!" >nul
+)
+
+echo.
+exit /b
